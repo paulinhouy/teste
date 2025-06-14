11bcc20ef857916d782f80b31aedd098805ff491
Paulo <paulofilhorocha22@gmail.com>
2025-06-13 23:12:52 -0300

    timerDate

--- script.js
+++ script.js
@@ -2,23 +2,36 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+
+function iniciaRelogio(){
+    timer = setInterval(function(){
+    segundos++
+    relogio.innerHTML = criarHoraSegundos(segundos);
+  },1000)
+}
+
+function criarHoraSegundos(segundos){
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR',{
+    hour12:false,
+    timeZone: 'GMT'
+  })
+}
+console.log(criarHoraSegundos(10))
+
+
+
+
 
 iniciar.addEventListener('click',function(event){
-setInterval(() => {
-  let date = new Date();
-  date.toLocaleDateString
-  console.log('iniciei');
-}, 1000);
+iniciaRelogio();
 })
 
 pausar.addEventListener('click',function(event){
-setTimeout(() => {
-    console.log('funcionei')
-  
-}, 1000); // 1000 milissegundos = 1
+clearInterval(timer);
 })
 
 zerar.addEventListener('click',function(event){
-alert('botão clicado');
 })
 
