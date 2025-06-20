04a3c6e1bf23c49e6b9c83f2242a789990d75692
Paulo <paulofilhorocha22@gmail.com>
2025-06-19 23:29:28 -0300

    apagarLi

--- toDo.js
+++ toDo.js
@@ -5,15 +5,38 @@ const ulTarefas = document.querySelector('.tarefas');
 function criarLi(){
     const li = document.createElement('li');
     return li;
+    
+}
 
+inputTarefa.addEventListener('keypress', function(e){
+    if(e.keyCode === 13){
+        if(!inputTarefa.value) return 
+        addLi(inputTarefa.value);
+        clearInput();
+    }
+    
+})
+
+function criaBotaoApagar(li){
+    li.innerHTML +=  ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML= 'Apagar';
+    li.appendChild(botaoApagar);
+}
+function clearInput(){
+inputTarefa.value = "";
+inputTarefa.focus();
 }
 
 function addLi(valueInput){
     const li = criarLi()
     li.innerHTML = inputTarefa.value;
     ulTarefas.appendChild(li)
+    clearInput();
+    criaBotaoApagar(li);
 }
 
 btnTarefa.addEventListener('click',function(){
     addLi();
+    clearInput();
 })
